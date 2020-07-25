1611b37749abd77625af7885bd9b6dbc6f5209c4
Zac Morgan <zacmorgan9829@yahoo.com>
2020-07-25 00:08:26 -0400

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,21 @@
+<?php
+
+	if (isset($_POST['submit'])) {
+		$name = $_POST['name'];
+		$subject = $_POST['subject'];
+		$mailFrom = $_POST['mail'];
+		$message = $_POST['message'];
+
+		$mailTo = "zacmorgan9829@yahoo.com";
+		$headers = "From: ".$mailFrom;
+		$txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+
+		mail($mailTo, $subject, $txt, $headers);
+		header("Location: index.php?mailsend");
+	}
+
+
+
+
+
+	
